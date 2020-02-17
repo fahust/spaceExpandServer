e7050577ae9fa25c1c3735f9e2396f7e2b3be6ff
unknown <k.dellova@coccinet.com>
2020-02-17 16:24:57 +0100

    first commit

---
+++ attack.js
@@ -0,0 +1,16 @@
+class Attack{
+    constructor(attackBy,attackTo,shipCat1,shipCat2,shipCat3,shipCat4,shipCat5) {
+        this.by = attackBy;
+        this.to = attackTo;
+        this.sc1 = shipCat1;
+        this.sc2 = shipCat2;
+        this.sc3 = shipCat3;
+        this.sc4 = shipCat4;
+        this.sc5 = shipCat5;
+    }
+
+    attackStart(){
+        
+    }
+
+}
---
+++ planet.js
@@ -0,0 +1,18 @@
+class Planet{
+    constructor(name,owner,timeBeforeAttack,timeRestAttack,attackedBy,shipCat1,shipCat2,shipCat3,shipCat4,shipCat5,defenseLevel,tradeLevel,scienceLevel) {
+        this.n = name; //name
+        this.o = owner; //owner
+        this.tba = timeBeforeAttack; //time before Attack
+        this.tra = timeRestAttack; //time rest attack
+        this.ab = attackedBy; //attack by
+        this.sc1 = shipCat1; //ship category 1
+        this.sc2 = shipCat2; //ship category 2
+        this.sc3 = shipCat3; //ship category 3
+        this.sc4 = shipCat4; //ship category 4
+        this.sc5 = shipCat5; //ship category 5
+        this.d = defenseLevel; //defense level
+        this.t = tradeLevel; //trade level
+        this.s = scienceLevel; //trade level
+    }
+
+}

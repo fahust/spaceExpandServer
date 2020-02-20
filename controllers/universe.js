ca49260ba799c2285911140546f1347caa4ba4a3
fahust <kevin.dellova@hotmail.fr>
2020-02-20 21:38:26 +0100

    reception correct by game maker

--- controllers/universe.js
+++ controllers/universe.js
@@ -3,16 +3,19 @@
 module.exports = app => {
     return {loadById,loadAll,deleteShip,addShip,launchAttack};
 
-    function loadById(req, res){//console.log(req)
-        res.json(app.universe.loadById(req.body.id));
+    function loadById(req, res){;
+        var body = JSON.parse(Object.keys(req.body))
+        res.json(app.universe.loadById(body.id));
     }
 
     function loadAll(req, res){
+        var body = JSON.parse(Object.keys(req.body))
         res.json(app.universe.loadAll());
         app.universe.addUtoAll();
     }
 
     function deleteShip(req, res){
+        var body = JSON.parse(Object.keys(req.body))
         app.universe.planets[req.body.id].deleteShip(req.body.cat,req.body.owner);
         res.json(app.universe.loadById(req.body.id));
     }

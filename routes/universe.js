0e1c654a9c72194e886069de6e5e5d0bc40b8820
unknown <k.dellova@coccinet.com>
2020-02-27 16:32:16 +0100

    add guild function and route,message F and R, and ressource for transport ship

--- routes/universe.js
+++ routes/universe.js
@@ -19,4 +19,33 @@ app.post('/universe/launchattack', app.controllers.universe.launchAttack);
 // 
 app.post('/universe/transferShip', app.controllers.universe.transferShip);
 
-};
+// 
+app.post('/universe/addRessourceByShipEvent', app.controllers.universe.addRessourceByShipEvent);
+// 
+app.post('/universe/loadUsersScore', app.controllers.universe.loadUsersScore);
+// 
+app.post('/universe/setUsersScore', app.controllers.universe.setUsersScore);
+// 
+app.post('/universe/addMessage', app.controllers.universe.addMessage);
+// 
+app.post('/universe/loadLastTenMessage', app.controllers.universe.loadLastTenMessage);
+// 
+app.post('/universe/addGuild', app.controllers.universe.addGuild);
+// 
+app.post('/universe/loadGuild', app.controllers.universe.loadGuild);
+// 
+app.post('/universe/addGuildRessource', app.controllers.universe.addGuildRessource);
+// 
+app.post('/universe/takeGuildRessource', app.controllers.universe.takeGuildRessource);
+// 
+app.post('/universe/addScore', app.controllers.universe.addScore);
+// 
+app.post('/universe/joinGuild', app.controllers.universe.joinGuild);
+// 
+app.post('/universe/invitMember', app.controllers.universe.invitMember);
+// 
+app.post('/universe/kickMember', app.controllers.universe.kickMember);
+// 
+app.post('/universe/upGradeMember', app.controllers.universe.upGradeMember);
+
+}
